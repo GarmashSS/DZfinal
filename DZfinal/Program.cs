ae40a6370ae01b90f8900bf9d7b644e224b218a6
Sergey <gss86@inbox.ru>
2023-02-18 16:07:20 +0300

    задача

--- DZfinal/Program.cs
+++ DZfinal/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[]array = {"Ростов", "Москва", ":-)", "02", "20"};
+for (int i = 0; i< array.Length; i++)
+{
+    if (array[i].Length <=3)
+   {
+    Console.WriteLine(array[i]);
+   }
+}
